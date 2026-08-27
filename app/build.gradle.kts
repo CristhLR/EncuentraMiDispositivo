4c616081eec3c5ba49e86250f8ae06e22c75a841
CristhLR <134649141+CristhLR@users.noreply.github.com>
2026-08-27 18:11:06 -0500

    fix: usar versión de Compose compatible con Android 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ kotlin {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2026.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2026.06.01"))
     implementation("androidx.activity:activity-compose:1.11.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
